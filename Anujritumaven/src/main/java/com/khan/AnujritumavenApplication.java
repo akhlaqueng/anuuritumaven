3263b00729034cb7366062df6457bee91e24f737
Akhlaque <akhlaquekhan_mca@rediffmail.com>
2021-02-13 21:45:32 +0530

    First Commit

---
+++ Anujritumaven/src/main/java/com/khan/AnujritumavenApplication.java
@@ -0,0 +1,13 @@
+package com.khan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnujritumavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnujritumavenApplication.class, args);
+	}
+
+}
